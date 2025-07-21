3158d5ee9dd357a4e24376aa2357b6f423e92c83
snupinel <63478081+pineldeer@users.noreply.github.com>
2025-07-21 11:03:26 +0900

    GreetingPage UI아아아아아주살짝 다듬음

--- app/src/main/java/com/example/dailysummary/pages/initialPages/GreetingPage.kt
+++ app/src/main/java/com/example/dailysummary/pages/initialPages/GreetingPage.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
@@ -30,19 +31,25 @@ fun GreetingPage(navController: NavController){
         verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
         Text(
             text = "Dialock에 오신 걸 환영합니다!",
-            style = MaterialTheme.typography.headlineSmall,
+            style = MaterialTheme.typography.headlineLarge,
             fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primary
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = "당신의 하루를 기록할 준비가 되셨나요?",
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onBackground,
+            color = MaterialTheme.colorScheme.primary,
             textAlign = TextAlign.Center
         )
+
+        Spacer(modifier = Modifier.height(48.dp))
+
+        Text(
+            text = "저는 당신을 위한 자물쇠.\n" +
+                    "일기가 저를 여는 열쇠에요.\n" +
+                    "당신의 하루를 기록할 준비가 되셨나요?",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center,
+            lineHeight = 24.sp
+        )
+
+        Spacer(modifier = Modifier.height(48.dp))
 
         Button(
             modifier = Modifier
